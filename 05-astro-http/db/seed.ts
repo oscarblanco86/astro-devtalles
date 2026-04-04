773dbac5224fed3971e89d8b879adbb459a20164
Oscar Blanco <oscarmb86@hotmail.com>
2026-04-04 10:18:39 -0600

    added clients db structure

--- 05-astro-http/db/seed.ts
+++ 05-astro-http/db/seed.ts
@@ -1,7 +1,39 @@
-import { db } from 'astro:db';
+import { db, Clients } from 'astro:db';
+
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Clients).values([
+		{
+			id: 1,
+			name: "Oscar",
+			age: 40,
+			isActive: true,
+		},
+		{
+			id: 2,
+			name: "Palencia",
+			age: 43,
+			isActive: true,
+		},
+		{
+			id: 3,
+			name: "Fernando",
+			age: 41,
+			isActive: false,
+		},
+		{
+			id: 4,
+			name: "Gerson",
+			age: 39,
+			isActive: true,
+		},
+		{
+			id: 5,
+			name: "Carlos",
+			age: 42,
+			isActive: true,
+		},
+	])
 	console.log('Seed executed')
 }
--- 05-astro-http/src/pages/api/posts/index.ts
+++ 05-astro-http/src/pages/api/posts/index.ts
@@ -1,12 +1,38 @@
-import { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import type { APIRoute } from 'astro';
+import { getCollection, getEntry } from 'astro:content';
 
-export const GET: APIRoute = async ({params, request}) => {
-    const posts = await getCollection('blog')
-    return new Response(JSON.stringify(posts), {
-        status:200,
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get('slug');
+
+  if (slug) {
+    const post = await getEntry('blog', slug);
+
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
         headers: {
-            "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
+  }
+
+  const posts = await getCollection('blog');
+
+  return new Response(JSON.stringify(posts), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 };
