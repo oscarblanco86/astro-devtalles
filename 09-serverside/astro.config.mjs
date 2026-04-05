b322a2c9232cd026dd2cb514b6f291d073174462
Oscar Blanco <oscarmb86@hotmail.com>
2026-04-04 21:54:15 -0600

    added node for server side rendering, setted as static

--- 09-serverside/astro.config.mjs
+++ 09-serverside/astro.config.mjs
@@ -4,8 +4,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  output:'static',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
--- 09-serverside/src/pages/api/posts/index.ts
+++ 09-serverside/src/pages/api/posts/index.ts
@@ -1,10 +1,12 @@
 import type { APIRoute } from "astro";
 import { getCollection, type CollectionEntry } from 'astro:content';
 
+export const prerender = false
 
 export const GET: APIRoute = async ({ params, request }) => {
     const posts = await getCollection('blog')
-    // console.log(posts)
+    console.log('params', params)
+    console.log(request)
     return new Response(JSON.stringify(posts), {
         status: 200,
         headers: {
