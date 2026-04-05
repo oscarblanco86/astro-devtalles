48292bea2f427f711105bf8c46617b9f9319d684
Oscar Blanco <oscarmb86@hotmail.com>
2026-04-04 22:48:09 -0600

    added slug search in query parameters

--- 09-serverside/src/pages/api/posts/index.ts
+++ 09-serverside/src/pages/api/posts/index.ts
@@ -1,12 +1,33 @@
 import type { APIRoute } from "astro";
-import { getCollection, type CollectionEntry } from 'astro:content';
+import { getCollection, getEntry, type CollectionEntry } from 'astro:content';
 
 export const prerender = false
 
 export const GET: APIRoute = async ({ params, request }) => {
+
+    const url = new URL(request.url)
+    const slug = url.searchParams.get('slug')
+
+    if (slug) {
+        const post = await getEntry('blog', slug)
+        if (post) {
+            return new Response(JSON.stringify(post), {
+                status: 200,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        } else {
+            return new Response(JSON.stringify({msg: `Post ${slug} Not Found`}), {
+                status: 404,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+        }
+
+    }
     const posts = await getCollection('blog')
-    console.log('params', params)
-    console.log(request)
     return new Response(JSON.stringify(posts), {
         status: 200,
         headers: {
